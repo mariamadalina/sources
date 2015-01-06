6a1b14ac5f7ea2f100bb2520e4221f0585e71022
mariamadalina <madalinamazilu@yahoo.com>
2015-01-07 00:28:20 +0200

    empty

---
+++ csharp/bob/Bob.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism.bob
+{
+    class Bob
+    {
+        public interface IAnswer
+        {
+            public string Answer { get;}
+
+        }
+        public class Question:IAnswer
+        {
+            private readonly string _answer = "Sure.";
+
+        }
+        public class Yelling : IAnswer
+        {
+            private readonly string _answer = "Whoa, chill out!";
+        }
+
+        public class Neutral : IAnswer
+        {
+            private readonly string _answer = "Fine. Be that way!";
+        }
+
+        public class Anything : IAnswer
+        {
+            private readonly string _answer = "Whatever.";
+        }
+
+        public string Hey(string input)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ csharp/leap/Year.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class Year
+    {
+        public static bool IsLeap(int year)
+        {
+          if (year%4 !=0)
+          {
+              return false;
+          }
+          if (year %100 !=0)
+          {
+              return true;
+          }
+
+          if (year%400 !=0)
+          {
+              return false;
+          }
+          return true;
+        }
+    }
+
